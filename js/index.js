4cccb3125fd0720e1f2b66fff724bacdb8ca3bac
Gregory Hawman <g.hawman@gmail.com>
2020-02-12 12:51:16 -0800

    got my first event working.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+const busImg = document.querySelector(".intro img")
+console.log(busImg);
+
+busImg.addEventListener("mouseenter", () => {
+    busImg.style.transform = "scale(1.2)";
+    busImg.style.transition = "transform 1s";
+  })
+  busImg.addEventListener("mouseleave", () => {
+    busImg.style.transform = "scale(1)";
+  })
