6fc0e2b6dd609983fe8f737c50fb8f587197793a
Gregory Hawman <g.hawman@gmail.com>
2020-02-12 12:54:44 -0800

    got my second event working. clicking on the intro changes the bg color and text color.

--- js/index.js
+++ js/index.js
@@ -2,10 +2,19 @@
 const busImg = document.querySelector(".intro img")
 console.log(busImg);
 
-busImg.addEventListener("mouseenter", () => {
-    busImg.style.transform = "scale(1.2)";
-    busImg.style.transition = "transform 1s";
+busImg.addEventListener('mouseenter', () => {
+    busImg.style.transform = 'scale(1.2)';
+    busImg.style.transition = 'transform 1s';
   })
-  busImg.addEventListener("mouseleave", () => {
-    busImg.style.transform = "scale(1)";
+  busImg.addEventListener('mouseleave', () => {
+    busImg.style.transform = 'scale(1)';
   })
+
+  document.querySelectorAll('.intro').forEach(e => {
+    e.addEventListener('click', function() {
+      e.style.backgroundColor = 'blue';
+      e.style.color = 'white';
+    })
+  })
+
+  
