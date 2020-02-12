d0cc05a6248d34e4d4b975485caa7e11de3a675c
Gregory Hawman <g.hawman@gmail.com>
2020-02-12 13:35:06 -0800

    got up to six events.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
 // Your code goes here
+document.querySelector('nav');
+
+// 1
 const busImg = document.querySelector(".intro img")
 console.log(busImg);
 
@@ -7,9 +10,11 @@ busImg.addEventListener('mouseenter', () => {
     busImg.style.transition = 'transform 1s';
   })
   busImg.addEventListener('mouseleave', () => {
-    busImg.style.transform = 'scale(1)';
+    
   })
 
+
+// 2
   document.querySelectorAll('.intro').forEach(e => {
     e.addEventListener('click', function() {
       e.style.backgroundColor = 'blue';
@@ -17,14 +22,45 @@ busImg.addEventListener('mouseenter', () => {
     })
   })
 
+
+// 3 & 4
   const changeImg = document.querySelectorAll('.img-content img');
   console.log(changeImg[0]);
 changeImg[0].addEventListener('dblclick', () => {
   changeImg[0].src='img/fun.jpg';
-})
+});
+changeImg[0].addEventListener('mouseleave', () => {
+    changeImg[0].src = 'img/adventure.jpg';
+  });
 
-const changeImg2 = document.querySelectorAll('.img-content img');
   console.log(changeImg[1]);
-changeImg2[1].addEventListener('dblclick', () => {
-  changeImg2[1].src='img/adventure.jpg';
+changeImg[1].addEventListener('dblclick', () => {
+  changeImg[1].src='img/adventure.jpg';
 })
+changeImg[1].addEventListener('mouseleave', () => {
+    changeImg[1].src = 'img/fun.jpg';
+  });
+
+
+// 5
+document.querySelectorAll('.img-content img').forEach(e => {
+    e.addEventListener('mouseenter', () => {
+        e.style.transform = 'scale(1.2)';
+        e.style.transition = 'transform 1s';
+    });
+    e.addEventListener('mouseleave', () => {
+        e.style.transform = 'scale(1)';
+        e.style = 'z-index(0)';
+    });
+});
+
+
+// 6
+const pick = document.querySelectorAll('.destination').forEach(e => {
+    document.querySelectorAll('.btn').forEach(e => {
+        e.addEventListener('click', function() {
+          pick.style.backgroundColor = 'blue';
+          pick.style.color = 'white';
+        })
+      })
+});
